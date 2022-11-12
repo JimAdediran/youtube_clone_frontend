41b593865d7bb5e59a05a786ddc2c93fd8c3d4d1
Jimmy Adediran <jimmy@Jimmys-MacBook-Pro.local>
2022-11-12 15:17:59 -0500

    added  urlHost.js + updated BASE_URL for AuthContext.js

---
+++ src/urlHost.js
@@ -0,0 +1,4 @@
+export const URL_HOST =
+  process.env.NODE_ENV === "production"
+    ? "https://cors-everywhere-me.herokuapp.com/http://youtubeclonebackend-env.eba-c3hpncuk.us-east-1.elasticbeanstalk.com"
+    : "http://127.0.0.1.8000";
