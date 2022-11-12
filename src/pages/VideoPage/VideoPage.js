c69ad1b14fb6c2e285634255b5d090f854fea05b
Jimmy Adediran <jimmy@Jimmys-MacBook-Pro.local>
2022-11-12 17:27:40 -0500

    updated VideoPage

--- src/pages/VideoPage/VideoPage.js
+++ src/pages/VideoPage/VideoPage.js
@@ -2,7 +2,6 @@ import React, { useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import axios from "axios";
 import { Link } from "react-router-dom";
-import AddComment from "../../components/AddComment/AddComment";
 import VideoDisplay from "../../components/VideoDisplay/VideoDisplay";
 
 
